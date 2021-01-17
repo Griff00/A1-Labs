17c43cc8f83caf5a68942ac4cea1b411e779e902
Griff00 <50583567+Griff00@users.noreply.github.com>
2021-01-17 18:15:23 +0000

    Update simple_count.py
    Merged contamination

--- simple_count.py
+++ simple_count.py
@@ -27,24 +27,35 @@ frame_y_bottom = 1
 #g: made below into a dictionary, so it keeps meaningful names while storing the same info
 frame_cutoffs = {"xl":frame_x_left,"xr":frame_x_right,"yt":frame_y_top,"yb":frame_y_bottom}
 
+#j: lower & upper pixel number limits for removing contamination
+lower_pix_limit = 1
+upper_pix_limit = 50
 
 class Source:
     #g: not sure if this is the right place for this- we can make a list of the sources outside their class.
     #   would take plenty of restructuring so will not do it now, but smth to think abt
     source_list = []
+    galaxy_list = []
+    
     def __init__(self,identifier,location,count):
                  
+        self.num_pixels = 1
         self.identifier = identifier
         self.pixel_coords = [location]
         self.pixel_counts = [count] 
         Source.source_list.append(self)
+        Source.galaxy_list.append(self)
         
     def add_pixel(self,location,count):
+        self.num_pixels +=1
         self.pixel_coords.append(location)
         self.pixel_counts.append(count)
         
     def get_pixel_coords(self):
         return self.pixel_coords
+
+    def get_num_pixels(self):
+        return self.num_pixels
         
 
 def mask_frame(mask,frame_cutoffs):
@@ -142,11 +153,28 @@ def source_handler(data,x,y,mask,dimensions,count,source_map):
         source_map[y][x] = count
     return count, source_map
 
+def contam_removal(mask, source_map,lower_pix_limit, upper_pix_limit):
+    print("running")
+    galaxy_map = source_map.copy()
+    for ob in Source.galaxy_list:
+        if ob.get_num_pixels() <= lower_pix_limit or ob.get_num_pixels() >= upper_pix_limit:
+            Source.galaxy_list.remove(ob)
+            
+            for j in range (0,len(galaxy_map)):
+                for i in range(0,len(galaxy_map[0])):
+                    if galaxy_map[j][i] == ob.identifier:
+                        galaxy_map[j][i] = 0
+                        mask[j][i] = False
+                        print("here")
+                        
+    return galaxy_map, mask
+
 mask = mask_frame(mask, frame_cutoffs)
 count, mask, source_map = source_detection(data,mask,background,dimensions,source_map,frame_cutoffs)
-
+galaxy_map, mask = contam_removal(mask, source_map,lower_pix_limit, upper_pix_limit)
 
 
 print("MASK: \n",mask)
 print("SOURCE MAP: \n", source_map)
 print("OBJECT COUNT:",count)
+print("GALAXY MAP: \n", galaxy_map)
